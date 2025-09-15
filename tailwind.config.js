533df10f84763ffbe8fc13e3e54e1dc716279256
Joseph Chu <hotcupofjoe2013@gmail.com>
2025-09-14 20:01:56 -0400

    Emptied the repository

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx}", "./docs/**/*.{js,jsx,md}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
