a218ea297e577f2b354ce0d18cff2657fbc977ba
Joseph Chu <hotcupofjoe2013@gmail.com>
2025-09-14 20:16:19 -0400

    chore: add Tailwind CSS and PostCSS config files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}", // Add paths to all your component files
+    "./docs/**/*.md",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
