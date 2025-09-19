72ad99609964f11e0397f82cea59e4a41c7a2e96
Joseph Chu <hotcupofjoe2013@gmail.com>
2025-09-19 16:22:12 -0400

    chore: updated deployment

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/accessible-react-ui-kit/demo/',
+  base: '/accessible-react-ui-kit/',
   plugins: [react()],
 })
